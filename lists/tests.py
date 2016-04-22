8138889a5a05451ec62d1f02484b942af36e031e
Johnson Ho <johnson_hugh@163.com>
2016-04-22 15:37:17 +0800

    第一次单元测试和url映射，傻瓜视图

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
-
-# Create your tests here.
+from django.core.urlresolvers import resolve
+from lists.views import home_page
+class HomePageTest(TestCase):
+    def test_root_urlresolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func,home_page)
