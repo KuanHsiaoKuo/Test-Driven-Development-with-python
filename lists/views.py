716abe9ab93c86c88cded8ecca52b3a131edf2fd
Johnson Ho <johnson_hugh@163.com>
2016-04-23 16:11:02 +0800

    在返回的网页中显示已存在的项目

--- lists/views.py
+++ lists/views.py
@@ -10,7 +10,5 @@ def home_page(request):
     #item = Item()
     #item.text = request.POST.get('item_text','')
     #item.save()
-
-    return render(request, 'home.html',{
-        'new_item_text':request.POST.get('item_text','')
-    })
+    items = Item.objects.all()
+    return render(request, 'home.html',{'items':items})
