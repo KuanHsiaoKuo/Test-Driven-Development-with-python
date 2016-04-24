52d53d3e831b4eb05f17e8b66e19945c8dd2dd82
Johnson Ho <johnson_hugh@163.com>
2016-04-24 21:35:00 +0800

    加入新建表单的视图和url映射，并简化首页测试，将表单action都指向/lists/new

--- lists/views.py
+++ lists/views.py
@@ -2,17 +2,12 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from lists.models import Item
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world')
-    #else:
-     #   new_item_text = ''
-    #item = Item()
-    #item.text = request.POST.get('item_text','')
-    #item.save()
-    items = Item.objects.all()
-    return render(request, 'home.html',{'items':items})
+    return render(request, 'home.html')
 def view_list(request):
     items = Item.objects.all()
 
     return render(request,'list.html',{'items':items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world')
