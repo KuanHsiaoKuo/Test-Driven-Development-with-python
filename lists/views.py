7a8b63ab0aed5ad95609d4690da913bfb616d52c
Johnson Ho <johnson_hugh@163.com>
2016-04-24 11:10:31 +0800

    新建功能测试类ListViewTest，并使用self.client.get直接获取服务器响应

--- lists/views.py
+++ lists/views.py
@@ -4,7 +4,7 @@ from lists.models import Item
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world')
     #else:
      #   new_item_text = ''
     #item = Item()
